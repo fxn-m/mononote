de5cda02f655cd329d6a71a875dc9a6a6aca3ba1
fxn-m <felix@kenobi.ai>
2025-11-17 09:51:46 +0000

    Update GitHub Pages deployment configuration and adjust base path in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: process.env.GITHUB_PAGES ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || ''}/` : '/',
+  base: process.env.GITHUB_PAGES 
+    ? `/${process.env.GITHUB_REPOSITORY?.split('/')[1] || 'mononote'}/` 
+    : '/',
   plugins: [
     react(),
   ],
